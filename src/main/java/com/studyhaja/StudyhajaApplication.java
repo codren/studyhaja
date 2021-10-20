0f594a9aeaee490a89dfe0d0c1c371b6431ffd12
codren <tkdrms0291@naver.com>
2021-10-20 21:03:39 +0900

    Project Setting

---
+++ src/main/java/com/studyhaja/StudyhajaApplication.java
@@ -0,0 +1,13 @@
+package com.studyhaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhajaApplication.class, args);
+	}
+
+}
