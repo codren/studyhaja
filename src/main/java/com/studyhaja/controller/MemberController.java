d0241821bd2867cd41f0b5b9a62f731009e68a60
codren <tkdrms0291@naver.com>
2021-10-28 10:16:31 +0900

    Check email token

--- src/main/java/com/studyhaja/controller/MemberController.java
+++ src/main/java/com/studyhaja/controller/MemberController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/member")
@@ -23,6 +24,7 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
     private final JoinFormValidator joinFormValidator;
 
     // 이메일 또는 닉네임 중복 검증
@@ -50,4 +52,28 @@ public class MemberController {
         memberService.saveMember(joinFormDto);
         return "redirect:/";
     }
+
+    // 이메일 검증 요청
+    @GetMapping("/email/check-token")
+    public String checkEmailToken(String token, String email, Model model) {
+
+        String view = "/member/emailCheckResult";
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            model.addAttribute("error", "wrongEmail");
+            return view;
+        }
+        if (!token.equals(member.getEmailToken())) {
+            model.addAttribute("error", "wrongToken");
+            return view;
+        }
+
+        member.setEmailVerified(true);
+        member.setJoinedTime(LocalDateTime.now());
+        model.addAttribute("nickname", member.getNickname());
+        model.addAttribute("numberOfMember", memberRepository.count());
+        return view;
+    }
 }
+
