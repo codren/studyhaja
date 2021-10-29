5aa88d3814cfe8193a8fd3a5f2e029aa2f886a9c
codren <tkdrms0291@naver.com>
2021-10-29 18:34:27 +0900

    Add NPM front library(modules)

--- src/main/java/com/studyhaja/config/SecurityConfig.java
+++ src/main/java/com/studyhaja/config/SecurityConfig.java
@@ -21,7 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
