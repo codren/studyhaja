c237cba31fbfb5ff3c24c6ab046ccc7a3a2340d4
codren <tkdrms0291@naver.com>
2021-11-04 09:14:01 +0900

    RememberMe

--- src/main/java/com/studyhaja/config/SecurityConfig.java
+++ src/main/java/com/studyhaja/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.studyhaja.config;
 
 import com.studyhaja.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    MemberService memberService;
+
+    @Autowired private MemberService memberService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,6 +36,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+
+                // rememberMe 기능으로 로그인을 수행하기 위함
+                .userDetailsService(memberService)
+
+                // Username, token, series 값을 DB에 저장하고 비교하기 위한 인터페이스 객체
+                .tokenRepository(tokenRepository());
     }
 
     @Override
@@ -50,5 +64,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+
+    }
 
 }
