8dc31058492f217ae9f48dab247184d99c6b684e
codren <tkdrms0291@naver.com>
2021-11-18 22:06:17 +0900

    Fix Tag Domain

--- src/main/java/com/studyhaja/domain/Tag.java
+++ src/main/java/com/studyhaja/domain/Tag.java
@@ -17,7 +17,7 @@ public class Tag {
     @Column(name = "tag_id")
     private Long id;
 
-    @Column(name = "tag_name")
+    @Column(name = "tag_name", unique = true)
     private String tagName;
 
 }
